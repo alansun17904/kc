13d73cc0fdc128dc2945d026197338c8b1bcdf83
Alan Sun <asun17904@gmail.com>
2024-01-25 12:43:07 -0500

    fixed issue with gpt2 and added variable filename

--- attack.py
+++ attack.py
@@ -14,6 +14,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("model_name", type=str, help="hugging face model repo id")
 parser.add_argument("base_model_name", type=str, help="name of the base model")
 parser.add_argument("check_point_dir", type=str, help="directory to store check points")
+parser.add_argument("output_filename", type=str, help="name of the output file")
 
 options = parser.parse_args()
 
@@ -23,11 +24,11 @@ BASE_MODEL_NAME = options.base_model_name
 CHECKPOINT_DIR = options.check_point_dir
 RECIPE = recipes.TextFoolerJin2019
 
-if BASE_MODEL_NAME == "gpt2":
-    tokenizer.pad_token = tokenizer.eos_token
-
 tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL_NAME)
 origin_model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
+
+if BASE_MODEL_NAME == "gpt2":
+    tokenizer.pad_token = tokenizer.eos_token
 
 model = HuggingFaceModelWrapper(origin_model, tokenizer)
 dataset = HuggingFaceDataset(DATASET, None, "test", shuffle=True)
@@ -36,7 +37,7 @@ attack_args = AttackArgs(
     num_examples=5000,
     # checkpoint_interval=1000,
     checkpoint_dir=CHECKPOINT_DIR,
-    log_to_csv=f"tf-{BASE_MODEL_NAME}-imdb-regularized.csv",
+    log_to_csv=f"{options.output_filename}.csv",
     query_budget=300,
     parallel=True,
     # num_workers_per_device=2,
